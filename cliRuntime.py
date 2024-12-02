b4ad0bd9d25b000cec86832760366e57bd1a9bed
Shad0w27 <100698447+Shad0w27@users.noreply.github.com>
2024-12-02 10:46:06 +0000

    added first two CLI Menus: Main Menu, Settings

--- cliRuntime.py
+++ cliRuntime.py
@@ -1,2 +1,42 @@
+from InquirerPy import inquirer
+
+def mainMenu(log):
+    while True:
+        choice = inquirer.select(
+            message="Main Menu:",
+            choices=[
+                "Start",
+                "Settings",
+                "Exit"
+            ]
+        ).execute()
+        
+        if choice == "Start":
+            log.info("Starting...")
+        elif choice == "Settings":
+            log.info("entering settings...")
+            settings(log)
+        elif choice == "Exit":
+            break
+        
+def settings(log):
+    while True:
+        choice = inquirer.select(
+            message="Settings:",
+            choices=[
+                "Running as Client/Host",
+                "Interface Mode",
+                "Back"
+            ]
+        ).execute()
+        
+        if choice == "Running as Client/Host":
+            pass
+        elif choice == "Interface Mode":
+            pass
+        elif choice == "Back":
+            break
+
 def runCLIRuntime(runAs, log):
-    log.info("launching CLI as " + runAs)
+    log.info("running CLI as " + runAs)
+    mainMenu(log)
